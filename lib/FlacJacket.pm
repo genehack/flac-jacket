abb3f16c8435fec7c254a04786f316969710ad5e
John SJ Anderson <genehack@genehack.org>
2012-12-29 19:23:31 -0800

    update command line flag

--- lib/FlacJacket.pm
+++ lib/FlacJacket.pm
@@ -129,7 +129,7 @@ sub ApplyTagsToMp3 {
     "--title=\"$tags->{title}\""            ,
     "--track=\"$tags->{track}\""            ,
     "--track-total=\"$tags->{numTracks}\""  ,
-    "--year=\"$tags->{year}\""              ,
+    "-Y=\"$tags->{year}\""              ,
   );
   if ( $tags->{disk} ) {
     push @options ,
